ae0798f0358fefb6e925d872804d27d2319fe9f3
srinu <sopparisrinu637@gmail.com>
2024-08-13 07:58:55 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var name ="Namaste Srinu Learning NodeJS";
+var a =10
+var b=20
+// console.log(name);
+// console.log(a+b)
+// var global = 1
+// console.log(globalThis)
+console.log(globalThis == global) // emty {} object
+// console.log(frames)
